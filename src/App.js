da247ecfeb8ae44d2c003277c3c3f296dcb8b910
Juraj Machac <jurajmchc@gmail.com>
2018-02-28 09:55:40 +0100

    Add stateful counters

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
+import './App.css'
 import React from 'react'
 import {NavLink, Route, Redirect} from 'react-router-dom'
 import SingleCounter from './counter_single/Counter'
+import TwoDependentCounters from './counter_single/TwoDependentCounters'
 import TwoCounters from './two_counters/TwoCounters'
 import MultipleCounters from './multiple_counters/MultipleCounters'
 import CounterState from './counter_state'
@@ -8,16 +10,22 @@ import CounterState from './counter_state'
 class App extends React.Component {
   render() {
     return (
-      <div>
-        <NavLink to="/stateful">Stateful counter</NavLink>
-        <NavLink to="/single-counter">Single counter</NavLink>
-        <NavLink to="/two-counters">Two counters</NavLink>
-        <NavLink to="/multiple-counters">Multiple counters</NavLink>
-        <Route path="/" exact render={() => <Redirect to={{pathname: '/single-counter'}} />} />
-        <Route path="/stateful" component={CounterState} />
-        <Route path="/single-counter" component={SingleCounter} />
-        <Route path="/two-counters" component={TwoCounters} />
-        <Route path="/multiple-counters" component={MultipleCounters} />
+      <div className="App">
+        <div className="App__links">
+          <NavLink to="/stateful">Stateful counter</NavLink>
+          <NavLink to="/single-counter">Single counter</NavLink>
+          <NavLink to="/two-dependent-counters">Two counters - dependent</NavLink>
+          <NavLink to="/two-counters">Two counters</NavLink>
+          <NavLink to="/multiple-counters">Multiple counters</NavLink>
+        </div>
+        <div className="App__content">
+          <Route path="/" exact render={() => <Redirect to={{pathname: '/single-counter'}} />} />
+          <Route path="/stateful" component={CounterState} />
+          <Route path="/single-counter" component={SingleCounter} />
+          <Route path="/two-dependent-counters" component={TwoDependentCounters} />
+          <Route path="/two-counters" component={TwoCounters} />
+          <Route path="/multiple-counters" component={MultipleCounters} />
+        </div>
       </div>
     )
   }
---
+++ src/counter_single/TwoDependentCounters.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Counter from './Counter'
+
+export default () => (
+  <div>
+    Redux counters connected to the same state
+    <Counter />
+    <Counter />
+  </div>
+)
